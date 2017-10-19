bf6e52849240d1ae84841fbba69061491fe8210e
Akash Kumar <32797050+AK-GameBooks@users.noreply.github.com>
2017-10-19 17:13:56 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@
 exports.port = 80;
 
 //serverIp for parsing of custom avatars, put your server's ip here
-exports.serverIp = 'imp-server.scalingo-80.psim.us';
+exports.serverIp = 'imp-server.scalingo.io-80.psim.us';
 
 // The server address - the address at which Pokemon Showdown should be hosting
 //   This should be kept set to 0.0.0.0 unless you know what you're doing.
